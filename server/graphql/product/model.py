c566597b5c93332b7be63f3cfdf6aacfc7d0fe7a
Nie Radu <nie.radu@gmail.com>
2020-01-22 18:01:17 +0200

    update graphql works

---
+++ server/graphql/product/model.py
@@ -0,0 +1,36 @@
+import os
+from sqlalchemy import *
+from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
+                            backref)
+from sqlalchemy.ext.declarative import declarative_base
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+engine = create_engine(
+    f"sqlite:///{BASE_DIR}/app-test.db", convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base = declarative_base()
+# We will need this for querying
+Base.query = db_session.query_property()
+
+
+class Department(Base):
+    __tablename__ = 'department'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    hired_on = Column(DateTime, default=func.now())
+    department_id = Column(Integer, ForeignKey('department.id'))
+    department = relationship(
+        Department,
+        backref=backref('employees',
+                        uselist=True,
+                        cascade='delete,all'))
