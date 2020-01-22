2f5ce4fafc7dff4cf93d60caaf35274294b80f88
Nie Radu <nie.radu@gmail.com>
2020-01-22 15:22:22 +0200

    promise fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from "redux";
+import {
+  createLogger
+} from "redux-logger";
+import thunk from "redux-thunk";
 
 import mathReducer from "./reducers/mathReducer";
 import contactReducer from "./reducers/contactsReducer";
 
 export default createStore(
-  combineReducers({ mathReducer, contactReducer }),
-  {},
-  applyMiddleware(createLogger())
+  combineReducers({
+    mathReducer,
+    contactReducer
+  }), {},
+  applyMiddleware(createLogger(), thunk)
 );
